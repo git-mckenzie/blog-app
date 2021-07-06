12ba8987e99e02645a0b58868a65fdf3408b4908
Jason McKenzie <jmac.atx@gmail.com>
2021-07-06 14:21:39 -0500

    adds seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+post = Post.create!(title: "Title 1", content: "This is a super awesome post.")
+post = Post.create!(title: "Title 2", content: "This is another really super awesome post.")
+post = Post.create!(title: "Title 3", content: "This is a THIRD super awesome post.")
+
+tag = Tag.create!(name: "new")
+tag = Tag.create!(name: "rad")
+tag = Tag.create!(name: "super cool")
